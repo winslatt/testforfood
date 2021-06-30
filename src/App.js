68cb3a47fa73f27c9c8cc1634fa4276dddaf7cd1
Lahwin <lahwin.l123@gmail.com>
2021-06-30 14:46:55 +0700

    test commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React fast food
         </a>
       </header>
     </div>
